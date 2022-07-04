7c5a97651451bfcbef69081fe9827bf87d95b132
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2022-07-04 22:15:15 +0900

    feat: Hello Controller 코드를 롬복으로 전환

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -25,5 +25,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk()) // HTTP Header의 상태 검증
                 .andExpect(content().string(hello)); // 응답 본문의 내용 검증
+    }
+    
+    @Test
+    public void return_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+        
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name) 
+                    .param("amount", String.valueOf(amount))) // param은 값으로 string만 허용
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // $를 기준으로 필드명을 명시
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
