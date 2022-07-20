85a7ea0d8b6393596fd40da13b47985fb3253143
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2022-07-20 15:08:31 +0900

    feat: 게시글 등록 API 테스트

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.springboot.web.dto;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ public class PostsSaveRequestDto {
     private String content;
     private String author;
 
+    @Builder
     public PostsSaveRequestDto(String title, String content, String author){
         this.title = title;
         this.content = content;
