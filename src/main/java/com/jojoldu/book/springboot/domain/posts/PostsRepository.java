28761f4c6f9066fde1d44fc09fb3dc810285d278
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2022-07-24 01:44:03 +0900

    feat: 게시글 전체 조회 화면 만들기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,11 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{ // 기본적 CRUD 자동 생성
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
